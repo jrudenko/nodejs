f64c1d3e177168080b10e1a282f5e8045ab8c177
Julia Rudenko <yrudenko@i.ua>
2023-02-28 20:53:40 +0200

    hw01

---
+++ README.md
@@ -0,0 +1,20 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+node index.js --action list
+https://monosnap.com/file/BhKV1r01Giu7hRAUfPjzXHcJGJAdh7
+
+# Получаем контакт по id
+node index.js --action get --id 5
+https://monosnap.com/file/3X1CU55nlAyyRhd4JIrNyMADa8mCR8
+
+# Добавялем контакт
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/jpSjxRGwgrjnVPadVaaOXti31oE32s
+
+
+
+# Удаляем контакт
+node index.js --action remove --id=3
+https://monosnap.com/file/AVw238nMfm23qrJtFJQ0NRM8eOB01E
+
+# listContacts
+https://monosnap.com/file/OdQ17hWFQMDfHYasfMjtxsd3bQoypd
---
+++ contacts.js
@@ -0,0 +1,65 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4 } = require("uuid");
+
+require("colors");
+
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    return result;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const removedContact = contacts.findIndex((item) => item.id === contactId);
+    const newContactList = contacts.filter((item) => item.id == !contactId);
+    fs.writeFile(contactsPath, JSON.stringify(newContactList));
+    if (removedContact >= 0) {
+      contacts.splice(removedContact, 1);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+      console.log(`Contact id "${contactId}" deleted successfully!`);
+    } else {
+      console.log(`contact with id #${contactId} is not in the database`);
+    }
+    return removedContact;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContacts = { name, email, phone, id: v4() };
+    contacts.push(newContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContacts;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
